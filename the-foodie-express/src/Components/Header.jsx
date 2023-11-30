f99e119b1317ef88a7037a3113a4a7bc14bc48f2
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2023-11-30 14:45:54 +0530

    bar added

--- the-foodie-express/src/Components/Header.jsx
+++ the-foodie-express/src/Components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { NavLink } from 'react-router-dom'
 const Header = ({ dark }) => {
 
     return (
@@ -7,8 +7,8 @@ const Header = ({ dark }) => {
             <div className='text-2xl font-bold head tracking-wider text-[#de9c37]'>The Foodie Express</div>
             <div>
                 <ul className='flex gap-6 text-lg font-semibold tracking-wide text-[#2d2e32]' >
-                    <li>Home</li>
-                    <li>About</li>
+                    <NavLink to={'home'}><li>Home</li></NavLink>
+                    <NavLink to={'about'}><li>About</li></NavLink>
                     <li>Contact</li>
                 </ul>
             </div>
--- the-foodie-express/src/Components/Shimmer.jsx
+++ the-foodie-express/src/Components/Shimmer.jsx
@@ -2,28 +2,41 @@ import React from 'react'
 
 const ShimmerCard = () => {
     return (
+
+
         <div className='w-[250px] h-96 gap-1 flex flex-col transition-all'>
-            <div className='w-[230px] h-48 rounded-2xl bg-[#8c8f8f]' ></div>
-            <div className='w-[50px] h-10 rounded-2xl bg-[#8c8f8f]'></div>
-            <div className='w-[40px] h-5 rounded-2xl bg-[#8c8f8f]'></div>
-            <div className='w-[40px] h-5 rounded-2xl bg-[#8c8f8f]'></div>
+            <div className='w-[230px] h-48 rounded-2xl bg-[#d3d5d5]' ></div>
+            <div className='w-[50px] h-10 rounded-2xl bg-[#d3d5d5]'></div>
+            <div className='w-[40px] h-5 rounded-2xl bg-[#d3d5d5]'></div>
+            <div className='w-[40px] h-5 rounded-2xl bg-[#d3d5d5]'></div>
         </div>
+
     )
 }
 const Shimmer = () => {
     return (
-        <div className='flex justify-center mt-10 flex-wrap gap-8 w-[80%] ml-[50%] -translate-x-[50%]'>
-            <ShimmerCard />
-            <ShimmerCard />
-            <ShimmerCard />
-            <ShimmerCard />
-            <ShimmerCard />
-            <ShimmerCard />
-            <ShimmerCard />
-            <ShimmerCard />
-            <ShimmerCard />
-
+        <div className='flex flex-col gap-5 mt-16'>  <div className='flex justify-center gap-10 mt-10'>
+            <div className='w-28 aspect-square rounded-full bg-[#d3d5d5]'></div>
+            <div className='w-28 aspect-square rounded-full bg-[#d3d5d5]'></div>
+            <div className='w-28 aspect-square rounded-full bg-[#d3d5d5]'></div>
+            <div className='w-28 aspect-square rounded-full bg-[#d3d5d5]'></div>
+            <div className='w-28 aspect-square rounded-full bg-[#d3d5d5]'></div>
+            <div className='w-28 aspect-square rounded-full bg-[#d3d5d5]'></div>
         </div>
+            <div className='flex justify-center mt-10 flex-wrap gap-8 w-[80%] ml-[50%] -translate-x-[50%]'>
+
+                <ShimmerCard />
+                <ShimmerCard />
+                <ShimmerCard />
+                <ShimmerCard />
+                <ShimmerCard />
+                <ShimmerCard />
+                <ShimmerCard />
+                <ShimmerCard />
+                <ShimmerCard />
+
+            </div></div>
+
     )
 }
 
