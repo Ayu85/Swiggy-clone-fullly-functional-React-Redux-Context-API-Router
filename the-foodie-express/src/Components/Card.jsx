e7b761ba1a39e9521e8788c57f81f69402a714f5
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-09 12:35:53 +0530

    deal stamp added

--- the-foodie-express/src/Components/Card.jsx
+++ the-foodie-express/src/Components/Card.jsx
@@ -3,7 +3,7 @@ import star from "../assets/star.png"
 import { imageAPI } from '../utils/Constants'
 import ThemeContext from '../utils/themeContext'
 import { SlLocationPin } from "react-icons/sl";
-const Card = ({ name, areaName, avgRating, sla, cuisines, cloudinaryImageId, avgRatingString }) => {
+const Card = ({ name, areaName, avgRating, sla, cuisines, cloudinaryImageId, aggregatedDiscountInfoV3 }) => {
     const { theme } = useContext(ThemeContext)
     const dark = {
         background: "#3A3B3C",
@@ -14,12 +14,13 @@ const Card = ({ name, areaName, avgRating, sla, cuisines, cloudinaryImageId, avg
         transition: "all .2s ease-in"
     }
     return (
-        <div className='w-[250px] h-[300px]  flex flex-col hover:scale-[1.1] transition-all px-3 py-3 rounded-xl ' style={theme.mode === "dark" ? dark : light}>
-            <div>
-                <img src={imageAPI + cloudinaryImageId} alt="" className='w-[230px] h-48 rounded-2xl' />
+        <div className=' w-[250px] h-[300px]  flex flex-col hover:scale-[1.1] transition-all px-3 py-3 rounded-xl ' style={theme.mode === "dark" ? dark : light}>
+            <div className='relative' >
+                <img src={imageAPI + cloudinaryImageId} alt="" className='w-[230px] h-48 rounded-2xl absolute' />
+                <h2 className='absolute bg-[#00000090] text-white px-3 py-2 rounded-2xl'>{aggregatedDiscountInfoV3.discountTag + aggregatedDiscountInfoV3.subHeader}</h2>
             </div>
-            <h1 className='text-lg font-bold tracking-wide text-[#414449] name mt-2 ml-3' style={theme.mode === "dark" ? { color: "#E4E6EB" } : { color: "#414449" }}>{name}</h1>
-            <div className='flex gap-2 font-semibold items-center text-[#414449] ml-3 text-base' style={theme.mode === "dark" ? { color: "#B0B3B8" } : { color: "#414449" }}>
+            <h1 className='text-lg font-bold tracking-wide text-[#414449] name mt-48 ml-3' style={theme.mode === "dark" ? { color: "#E4E6EB" } : { color: "#414449" }}>{name}</h1>
+            <div className='flex gap-2 font-semibold items-center text-[#414449] ml-3 text-base ' style={theme.mode === "dark" ? { color: "#B0B3B8" } : { color: "#414449" }}>
                 <h3 className='flex h-5 gap-1  items-center '><img src={star} alt="" className='w-5' style={theme.mode === "dark" ? { color: "#B0B3B8" } : { color: "#414449" }} />{avgRating} ▪️</h3>
                 <h3>{sla?.deliveryTime} MINS</h3>
             </div>
